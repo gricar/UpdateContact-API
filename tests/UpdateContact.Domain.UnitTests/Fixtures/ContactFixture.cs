759afe983cf62c96badce8cc56cb9fdb5b6f4b48
Gabriel Ricardo <gabriel.rodrigues.ricardo@outlook.com>
2025-03-24 19:50:10 -0300

    domain unit tests

---
+++ tests/UpdateContact.Domain.UnitTests/Fixtures/ContactFixture.cs
@@ -0,0 +1,23 @@
+﻿using Bogus;
+using UpdateContact.Domain.Entities;
+
+namespace UpdateContact.Domain.UnitTests.Fixtures;
+
+public class ContactFixture
+{
+    private readonly Faker _faker;
+
+    public ContactFixture()
+    {
+        _faker = new Faker();
+    }
+
+    public Contact CreateValidContact()
+    {
+        return Contact.Create(
+            _faker.Person.FirstName,
+            _faker.PickRandom(new[] { 11, 21, 31, 41 }),
+            _faker.Phone.PhoneNumber("#########"),
+            _faker.Person.Email);
+    }
+}
